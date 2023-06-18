8eea8da69d6b11a0580f2f3df051b52db28bea49
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2023-06-18 17:46:25 +0300

    add modal

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,22 @@
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    backdrop: document.querySelector(".backdrop"),
+    body: document.querySelector("body"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.backdrop.addEventListener("click", handleBackdropClick);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+
+  function handleBackdropClick(event) {
+    if (event.currentTarget === event.target) {
+      toggleModal();
+    }
   }
 })();
